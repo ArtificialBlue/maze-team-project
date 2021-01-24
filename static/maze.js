4ce60bb4a1c3e8a58611c05acc841278fb970677
Secundino Martinez <Dino.Martinez.514@gmail.com>
2021-01-23 20:26:58 -0800

    [added] Typescript code (and compiled JS) to draw our canvas, build walls, and allow player movement

--- static/maze.js
+++ static/maze.js
@@ -1,26 +1,194 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-const maze = document.querySelector('#maze-data').innerHTML.split('')
-const numCols = Math.sqrt(maze.length)
-const brickSize = canvas.width / numCols
-const bricks = []
-let col = 0
-let row = 0
-maze.forEach((bit) => {
-  if(bit === '1') {
-    const x = col * brickSize
-    const y = row * brickSize
-    bricks.push({x:x, x:y})
-  }
-  col++
-  if(col >= numCols) {
-    col = 0
-    row++
-  }
-})
-bricks.forEach((brick) => {
-  ctx.beginPath()
-  ctx.rect(brick.x, brick.y, brickSize, brickSize)
-  ctx.fillStyle = '#0040F5'
-  ctx.fill()
-})
+/* Upper level interface for all objects within our game
+interface RenderableObject {
+  // Properties for all renderable objects
+   position:Vector;
+   size:Vector;
+   color:string;
+   movable:boolean;
+
+   // We need to be able to change the position
+   // Always return the new position
+   updatePosition(delta:Vector):Vector;
+
+   // Check for collision between this object and a specified object
+   // Based on both objects positions and sizes
+   checkCollision(target:RenderableObject):boolean;
+
+   // Every drawn object needs to have some logic that draws it
+   // Dependency injection is the canvas context
+   draw(ctx:CanvasRenderingContext2D):void;
+}
+*/
+/* General vector class for physics calculations */
+var Vector = /** @class */ (function () {
+    // Instantiation methods
+    function Vector(x, y) {
+        this.x_component = x;
+        this.y_component = y;
+    }
+    Object.defineProperty(Vector.prototype, "x", {
+        // Accessors
+        get: function () {
+            return this.x_component;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Vector.prototype, "y", {
+        get: function () {
+            return this.y_component;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Vector.prototype, "mag", {
+        get: function () {
+            return Math.sqrt((this.x_component ^ 2) + (this.y_component ^ 2));
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Vector.prototype, "dir", {
+        get: function () {
+            return Math.atan(this.y_component / this.x_component);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // Modifiers
+    Vector.prototype.update = function (new_x, new_y) {
+        this.x_component = new_x;
+        this.y_component = new_y;
+    };
+    return Vector;
+}());
+/* This class will represent our base class for everything we draw on the screen */
+var Sprite = /** @class */ (function () {
+    // Instantiation
+    function Sprite(x, y, width, height, color, movable) {
+        this.position = new Vector(x, y);
+        this.size = new Vector(width, height);
+        this.color = color;
+        this.movable = movable;
+    }
+    Object.defineProperty(Sprite.prototype, "x", {
+        // Accessors
+        get: function () {
+            return this.position.x;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Sprite.prototype, "y", {
+        get: function () {
+            return this.position.y;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Sprite.prototype, "width", {
+        get: function () {
+            return this.size.x;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Sprite.prototype, "height", {
+        get: function () {
+            return this.size.y;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    // Modifiers
+    Sprite.prototype.updatePosition = function (delta) {
+        if (this.movable) {
+            var new_x = this.x + delta.x;
+            var new_y = this.y + delta.y;
+            this.position.update(new_x, new_y);
+            return this.position;
+        }
+    };
+    // Check for collisions
+    Sprite.prototype.checkCollision = function (target) {
+        if (this.position.x + this.size.x > target.position.x && this.position.x - 8 < target.position.x + target.size.x) {
+            if (this.position.y + this.size.y > target.position.y && this.position.y - 8 < target.position.y + target.size.y) {
+                return true;
+            }
+        }
+        return false;
+    };
+    // Render
+    Sprite.prototype.draw = function (ctx) {
+        var canvasWidth = ctx.canvas.clientWidth;
+        var canvasHeight = ctx.canvas.clientHeight;
+        // Draw the object using our instance properties
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    };
+    return Sprite;
+}());
+var MazeGame = /** @class */ (function () {
+    function MazeGame() {
+        var _this = this;
+        // Note: lamba syntax is required here to make sure the 'this' context persists through animation frames
+        this.run = function () {
+            _this.ctx.clearRect(0, 0, _this.canvas.width, _this.canvas.height);
+            _this.player.draw(_this.ctx);
+            _this.maze.forEach(function (brick) {
+                brick.draw(_this.ctx);
+            });
+            requestAnimationFrame(_this.run);
+        };
+        this.keyDownHandler = function (e) {
+            if (e.key === 'Right' || e.key === 'ArrowRight') {
+                _this.player.updatePosition(new Vector(10, 0));
+            }
+            else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+                _this.player.updatePosition(new Vector(-10, 0));
+            }
+            else if (e.key === 'Up' || e.key === 'ArrowUp') {
+                _this.player.updatePosition(new Vector(0, -10));
+            }
+            else if (e.key === 'Down' || e.key === 'ArrowDown') {
+                _this.player.updatePosition(new Vector(0, 10));
+            }
+        };
+        this.canvas = document.getElementById('maze-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.canvasBounds = [
+            new Sprite(-100, -100, 100, this.canvas.height + 100, 'none', false),
+            new Sprite(-100, -100, this.canvas.width + 100, 100, 'none', false),
+            new Sprite(this.canvas.width, -100, 100, this.canvas.height + 100, 'none', false),
+            new Sprite(-100, this.canvas.height, this.canvas.width + 100, 100, 'none', false)
+        ];
+        var mazeData = document.querySelector('#maze-data').innerHTML.split('');
+        var bricks = [];
+        var numCols = Math.sqrt(mazeData.length);
+        var brickSize = this.canvas.width / numCols;
+        this.player = new Sprite(0, 0, brickSize, brickSize, '#ff0000', true);
+        var col = 0;
+        var row = 0;
+        mazeData.forEach(function (bit) {
+            if (bit === '1') {
+                var x = col * brickSize;
+                var y = row * brickSize;
+                bricks.push(new Sprite(x, y, brickSize, brickSize, '#0040F5', false));
+            }
+            col++;
+            if (col >= numCols) {
+                col = 0;
+                row++;
+            }
+        });
+        this.maze = [new Sprite(50, 50, brickSize, brickSize, '#00405f', false)];
+    }
+    return MazeGame;
+}());
+var game = new MazeGame();
+document.addEventListener('keydown', game.keyDownHandler, false);
+game.run();
+//# sourceMappingURL=maze.js.map
---
+++ static/maze.ts
@@ -0,0 +1,193 @@
+/* Upper level interface for all objects within our game
+interface RenderableObject {
+  // Properties for all renderable objects
+   position:Vector;
+   size:Vector;
+   color:string;
+   movable:boolean;
+
+   // We need to be able to change the position
+   // Always return the new position
+   updatePosition(delta:Vector):Vector;
+
+   // Check for collision between this object and a specified object
+   // Based on both objects positions and sizes
+   checkCollision(target:RenderableObject):boolean;
+
+   // Every drawn object needs to have some logic that draws it
+   // Dependency injection is the canvas context
+   draw(ctx:CanvasRenderingContext2D):void;
+}
+*/
+
+/* General vector class for physics calculations */
+class Vector {
+    // Instance properties
+    x_component:number;
+    y_component:number;
+
+    // Instantiation methods
+    constructor(x:number, y:number) {
+        this.x_component = x;
+        this.y_component = y;
+    }
+
+    // Accessors
+    get x():number {
+        return this.x_component;
+    }
+
+    get y():number {
+        return this.y_component;
+    }
+
+    get mag():number {
+        return Math.sqrt((this.x_component ^ 2) + (this.y_component ^ 2));
+    }
+
+    get dir():number {
+        return Math.atan(this.y_component / this.x_component);
+    }
+
+    // Modifiers
+    update(new_x:number, new_y:number):void {
+        this.x_component = new_x;
+        this.y_component = new_y;
+    }
+}
+
+/* This class will represent our base class for everything we draw on the screen */
+class Sprite {
+    // Instance properties
+    position:Vector;
+    size:Vector;
+    color:string;
+    movable:boolean;
+
+    // Instantiation
+    constructor(x:number, y:number, width:number, height:number, color:string, movable:boolean) {
+        this.position = new Vector(x, y);
+        this.size = new Vector(width, height);
+        this.color = color;
+        this.movable = movable;
+    }
+
+    // Accessors
+    get x():number{
+        return this.position.x;
+    }
+
+    get y():number {
+        return this.position.y;
+    }
+
+    get width():number {
+        return this.size.x;
+    }
+
+    get height():number {
+        return this.size.y;
+    }
+
+    // Modifiers
+    updatePosition(delta:Vector):Vector {
+        if(this.movable) {
+            const new_x = this.x + delta.x;
+            const new_y = this.y + delta.y;
+            this.position.update(new_x, new_y);
+            return this.position;
+        }
+    }
+
+    // Check for collisions
+    checkCollision(target:Sprite):boolean {
+        if (this.position.x + this.size.x > target.position.x && this.position.x - 8 < target.position.x + target.size.x) {
+            if (this.position.y + this.size.y > target.position.y && this.position.y - 8 < target.position.y + target.size.y) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Render
+    draw(ctx:CanvasRenderingContext2D):void {
+        const canvasWidth = ctx.canvas.clientWidth;
+        const canvasHeight = ctx.canvas.clientHeight;
+
+        // Draw the object using our instance properties
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+class MazeGame {
+    // Instance properties
+    canvas:HTMLCanvasElement;
+    ctx:CanvasRenderingContext2D;
+    canvasBounds:Array<Sprite>;
+    maze:Array<Sprite>;
+    player:Sprite;
+
+    constructor() {
+        this.canvas = <HTMLCanvasElement> document.getElementById('maze-canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.canvasBounds = [
+            new Sprite(-100, -100, 100, this.canvas.height + 100,'none', false),
+            new Sprite(-100, -100, this.canvas.width + 100, 100,'none',false),
+            new Sprite(this.canvas.width, -100, 100, this.canvas.height + 100,'none',false),
+            new Sprite(-100, this.canvas.height, this.canvas.width + 100, 100, 'none',false)
+        ];
+        const mazeData:Array<string> = document.querySelector('#maze-data').innerHTML.split('');
+        const bricks:Array<Sprite> = [];
+        const numCols:number = Math.sqrt(mazeData.length);
+        const brickSize:number = this.canvas.width / numCols;
+        this.player = new Sprite(0,0, brickSize, brickSize, '#ff0000', true);
+        let col:number = 0;
+        let row:number = 0;
+        mazeData.forEach((bit) => {
+          if(bit === '1') {
+            const x:number = col * brickSize;
+            const y:number = row * brickSize;
+            bricks.push(new Sprite(x, y, brickSize, brickSize, '#0040F5',false));
+          }
+          col++
+          if(col >= numCols) {
+            col = 0
+            row++
+          }
+        });
+
+        this.maze = [new Sprite(50,50,brickSize,brickSize, '#00405f', false)];
+    }
+
+    // Note: lamba syntax is required here to make sure the 'this' context persists through animation frames
+    run = ():void => {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.player.draw(this.ctx);
+        this.maze.forEach((brick) => {
+          brick.draw(this.ctx);
+        });
+
+        requestAnimationFrame(this.run);
+    }
+
+    keyDownHandler = (e:KeyboardEvent):void => {
+       if (e.key === 'Right' || e.key === 'ArrowRight') {
+         this.player.updatePosition(new Vector(10, 0));
+       } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+           this.player.updatePosition(new Vector(-10, 0));
+       } else if (e.key === 'Up' || e.key === 'ArrowUp') {
+           this.player.updatePosition(new Vector(0, -10));
+       } else if (e.key === 'Down' || e.key === 'ArrowDown') {
+           this.player.updatePosition(new Vector(0, 10));
+       }
+   }
+}
+
+
+const game:MazeGame = new MazeGame();
+document.addEventListener('keydown', game.keyDownHandler, false);
+game.run();
