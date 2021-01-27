4fcbc6ca9da0b3eedde06a112d2032c010f8b4c7
Ali Shah <s.ashah@outlook.com>
2021-01-27 03:40:02 -0500

    [Update] Improved get_maze() readability

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-MAZE_FIELD_PATH = "./static/maze_fields"
+MZ_PATH = "./static/maze_fields"
 
 def get_maze(level):
     """Reads maze field data from file, removes newline chars,
     returns resultant bitstring."""
 
-    # filename = f"{MAZE_FIELD_PATH}/Dino.mz"
-    filename = f"{MAZE_FIELD_PATH}/{level}.mz"
+    filename = f"{MZ_PATH}/{level}.mz"
 
-    with open(filename, "r") as bitstring:
-        bitstring = bitstring.read()
-
-    bitstring = bitstring.replace("\n", "")  # Removes newlines
+    # Read and remove newlines
+    with open(filename, "r") as mz:
+        bitstring = mz.read().replace("\n", "")
 
     return(bitstring)
 
